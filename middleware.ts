5c77011ddc1a09c0a9899bdb1134b64d1f1c4588
NoorHasan92 <mdnoorhasan1720@gmail.com>
2026-08-03 22:39:51 +0530

    fix(middleware): exclude static files from authentication matcher

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,5 @@ import { authConfig } from "@/config/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)"],
 };
